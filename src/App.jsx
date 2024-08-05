552790780700d8e2dbbcdef78094d98aee935cac
benjolyman <benjolyman@hotmail.com>
2024-08-05 23:41:57 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import NavBar from "./components/NavBar"
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <NavBar/>
+    hello
+    </>
+  )
+}
+
+export default App
